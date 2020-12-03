12ca7c7c37ae9ac4c978a632b884fbbe85be119b
jhmny <58868135+jhmny@users.noreply.github.com>
2020-12-02 23:33:44 -0800

    idk lol

--- quartusProjv3/software/piano/obj/default/piano.d
+++ quartusProjv3/software/piano/obj/default/piano.d
@@ -1,1 +1,45 @@
-obj/default/piano.o: piano.c
+obj/default/piano.o: piano.c ../piano_bsp/system.h ../piano_bsp/linker.h \
+ ../piano_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../piano_bsp//HAL/inc/io.h ../piano_bsp//HAL/inc/alt_types.h \
+ ../piano_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../piano_bsp//HAL/inc/sys/alt_irq.h ../piano_bsp//HAL/inc/nios2.h \
+ ../piano_bsp//HAL/inc/alt_types.h \
+ ../piano_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../piano_bsp//drivers/inc/altera_up_avalon_ps2.h \
+ ../piano_bsp//HAL/inc/sys/alt_dev.h \
+ ../piano_bsp//HAL/inc/sys/alt_llist.h \
+ ../piano_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../piano_bsp//drivers/inc/altera_up_ps2_keyboard.h \
+ ../piano_bsp//drivers/inc/altera_up_avalon_ps2.h
+
+../piano_bsp/system.h:
+
+../piano_bsp/linker.h:
+
+../piano_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../piano_bsp//HAL/inc/io.h:
+
+../piano_bsp//HAL/inc/alt_types.h:
+
+../piano_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../piano_bsp//HAL/inc/sys/alt_irq.h:
+
+../piano_bsp//HAL/inc/nios2.h:
+
+../piano_bsp//HAL/inc/alt_types.h:
+
+../piano_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../piano_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../piano_bsp//HAL/inc/sys/alt_dev.h:
+
+../piano_bsp//HAL/inc/sys/alt_llist.h:
+
+../piano_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../piano_bsp//drivers/inc/altera_up_ps2_keyboard.h:
+
+../piano_bsp//drivers/inc/altera_up_avalon_ps2.h:
--- quartusProjv3/software/piano/piano.c
+++ quartusProjv3/software/piano/piano.c
@@ -7,11 +7,29 @@
 #include <stdio.h>
 #include "system.h"
 #include "altera_avalon_pio_regs.h"
-#include "altera_up_avalon_ps2_regs.h"
-#include "altera_up_avalon_audio_and_video_config_regs"
+#include "altera_avalon_timer_regs.h"
+#include "sys/alt_irq.h"
+#include "alt_types.h"
+#include "altera_up_avalon_ps2.h"
+#include "altera_up_ps2_keyboard.h"
+#include "sys/alt_dev.h"
 
 int main()
 {
-
+    alt_putstr("program start");
+    int ps2 = 1; //set type to keyboard
+    unsigned char psChar;
+    alt_up_ps2_init(ps2);
+    while (1) //infinite while loop to wait for ints && IORD_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE) == 0b01
+    {
+        alt_up_ps2_read_data_byte(ps2, &psChar);
+        if (psChar != "")
+        {
+            alt_putstr(psChar);
+            alt_putstr("state reset");
+        }
+    }
     return 0;
-}
+};
+
+//showa time
