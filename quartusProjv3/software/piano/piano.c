1f3e2099aacde9d24a63cb52505057f5702cf152
Megan Abundo <meganabundo23@gmail.com>
2020-12-02 22:29:48 -0800

    Update piano.c

--- quartusProjv3/software/piano/piano.c
+++ quartusProjv3/software/piano/piano.c
@@ -4,6 +4,11 @@
  *  Created on: Dec 1, 2020
  *      Author: Joshua
  */
+#include <stdio.h>
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include "altera_up_avalon_ps2_regs.h"
+#include "altera_up_avalon_audio_and_video_config_regs"
 
 void main()
 {
